1a59404e9bda8fa76194f55fffecf7727d254668
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-06 23:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ let cell1=newRow.insertCell()
 	let cell2=newRow.insertCell()
 
 	
-cell1.innerHTML="New Cell1"
-	cell2.innerHTML="New Cell2"
+cell1.innerHTML="NewCell1"
+	cell2.innerHTML="NewCell2"
 }
