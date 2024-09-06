f25398474f1a8c72fbcf2e063dd4ee741c1b72bb
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-06 23:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function insert_Row() {
  
 let table= document.getElementById('sampleTable')
 	
-let newRow=table.insertRow()
+let newRow=table.insertRow(0)
 	
 let cell1=newRow.insertCell(0)
 	let cell2=newRow.insertCell(1)
