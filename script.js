a37658033a26e14a01dc40756e407217ead56e88
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-06 23:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let table= document.getElementById('sampleTable')
 	
 let newRow=table.insertRow()
 	
-let cell1=newRow.insertCell()
-	let cell2=newRow.insertCell()
+let cell1=newRow.insertCell(0)
+	let cell2=newRow.insertCell(1)
 
 	
 cell1.innerHTML="NewCell1"
